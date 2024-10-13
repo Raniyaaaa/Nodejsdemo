6380ddcf8e3d0f4d3d873fd0b855fdfeb18515f1
Raniyaaaa <raniya182002@gmail.com>
2024-10-13 13:28:47 +0530

    array map example

--- play.js
+++ play.js
@@ -1,3 +1,6 @@
 let product=(a,b)=>a*b;
 console.log(product(2,3));
 
+const Arrayss=['apple', 'oranges' , ' ', 'mango', ' ' , 'lemon']
+const transform=Arrayss.map(item=>item===' '?'empty string':item)
+console.log(transform)
