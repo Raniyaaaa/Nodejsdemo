e9b1893c31a57f2960047a266d7ca0e1306317e1
Raniyaaaa <raniya182002@gmail.com>
2024-10-13 20:05:05 +0530

    writing contents according to the urls

--- app.js
+++ app.js
@@ -1,5 +1,28 @@
 const http =require('http');
 const server=http.createServer((req,res)=>{
-    console.log("My name is Raniya");
+    res.setHeader('Content-Type', 'text/html');
+    
+    if(req.url==='/home'){
+        res.write('<html>');
+        res.write('<head><title>Home</title></head>');
+        res.write('<body><h1>Welcome home</h1></body>');
+        res.write('</html>');
+        res.end();
+    }
+    else if(req.url==='/about'){
+        res.write('<html>');
+        res.write('<head><title>Home</title></head>');
+        res.write('<body><h1>Welcome to About Us page</h1></body>');
+        res.write('</html>');
+        res.end();
+    }
+    else if(req.url==='/node'){
+        res.write('<html>');
+        res.write('<head><title>Home</title></head>');
+        res.write('<body><h1>Welcome to my Node Js project</h1></body>');
+        res.write('</html>');
+        res.end();
+    }
 })
 server.listen(4000);
+
